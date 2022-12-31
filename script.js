225588d63dd1c168c5ef6e16705b54f2e57dceb8
Johngtka <johngtka@gmail.com>
2022-12-31 01:42:56 +0100

    .

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+window.onload = () => {
+    option()
+}
 const opt_container = document.querySelector('#menu')
+const tab = ["listing commands"]
+function option() {
+    for (let i = 1; i <= 6; i++) {
+        const opt = document.createElement('div')
+        opt.classList.add('option')
+        opt.setAttribute('id', i)
+        opt_container.appendChild(opt)
+    }
+}
