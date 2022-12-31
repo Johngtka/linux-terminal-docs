f57253e73bcb6d34bb18fde2bda34ce76be1b9d3
Johngtka <johngtka@gmail.com>
2022-12-31 18:11:02 +0100

    .

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.onload = () => {
-const tab = ["listing commands", "directory commands", "system commands", "hadrware commands", "files commands", "software commands"]
     option()
 }
 const opt_container = document.querySelector('#menu')
@@ -14,6 +13,7 @@ function render() {
 render()
 const opt1 = [document.querySelector('#option1'), document.querySelector('#option2'), document.querySelector('#option3'), document.querySelector('#option4'), document.querySelector('#option5'), document.querySelector('#option6')]
 function option() {
+    const tab = ["listing commands", "directory commands", "system commands", "hadrware commands", "files commands", "software commands"]
     for (let i = 0; i <= 6; i++) {
         opt1[i].innerHTML = '<p>' + tab[i].toUpperCase() + '</p>'
     }
