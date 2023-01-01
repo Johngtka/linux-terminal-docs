57a98eddb871e6e1caaed141582376bdfc87d435
Johngtka <johngtka@gmail.com>
2023-01-01 20:58:59 +0100

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Globalny uchwyt do elementu w którym mają się pojawić elementy
 const opt_container = document.querySelector('#menu')
 
-// funkcja renderująca(tworząca) elementy z odpowiednimi parametrami ( jak klasa i id(jest sterowane za pomocą pętli)) do opt_container
+// funkcja renderująca(tworząca) elementy z odpowiednimi parametrami (jak klasa i id(które jest sterowane za pomocą pętli)) do opt_container
 function render() {
     for (let i = 1; i <= 6; i++) {
         const opt = document.createElement('div')
@@ -15,7 +15,7 @@ render()
 // Globalna tablica z uchwytami do utworzonych elementów tworzonych w funkcji render()
 const opt1 = [document.querySelector('#option1'), document.querySelector('#option2'), document.querySelector('#option3'), document.querySelector('#option4'), document.querySelector('#option5'), document.querySelector('#option6')]
 
-// Funkcja która przypisuje przekonwertowany text do(CAPS LOCK) jako wartość z lokalnej tablicy do elementu opt1. Dodatkowo dodająca do elementów opt1 zdarzenie onclick ustawione na inną funkcję
+// Funkcja która przypisuje przekonwertowany text jako (CAPS LOCK) z lokalnej tablicy do elementu opt1. Dodatkowo dodaje do elementów opt1 zdażenie onclick ustawione na inną, niezależną funkcję
 function option() {
     const tab = ["listing commands", "directory commands", "system commands", "hardware commands", "files commands", "software commands"];
     for (let i = 0; i <= 6; i++) {
@@ -24,11 +24,17 @@ function option() {
     }
 }
 option()
-// Funkcja dopisana jako zdarzenie podczas procesu funkcji option(), wykonująca metode słuchacza zdarzeniowego który otworzy ospowiedni plik z pod folderu /subpages na tej samej karcie przeglądarki
+
+// Funkcja dopisana jako zdarzenie podczas procesu funkcji option(), wykonująca metode słuchacza zdarzeniowego który otworzy odpowiedni plik z podfolderu /subpages na tej samej karcie przeglądarki
 function eventclicker() {
     const sub = ["/subpages/ls.html", "/subpages/dir.html", "/subpages/os.html", "/subpages/hw.html", "/subpages/fil.html", "/subpages/soft.html"]
     // for (let i = 0; i <= 6; i++) {
     //     opt1[i].addEventListener("click", () => { window.open(sub[i], "_self") })
     // }
     opt1[0].addEventListener("click", () => { window.open(sub[0], "_self") })
+    opt1[1].addEventListener("click", () => { window.open(sub[1], "_self") })
+    opt1[2].addEventListener("click", () => { window.open(sub[2], "_self") })
+    opt1[3].addEventListener("click", () => { window.open(sub[3], "_self") })
+    opt1[4].addEventListener("click", () => { window.open(sub[4], "_self") })
+    opt1[5].addEventListener("click", () => { window.open(sub[5], "_self") })
 }
