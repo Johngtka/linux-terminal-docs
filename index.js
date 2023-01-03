25710bdbf73b4e80b4bdb3449f36b78b5df1bae5
Johngtka <johngtka@gmail.com>
2023-01-03 21:29:09 +0100

    .

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const opt_container = document.querySelector('#menu')
 // funkcja renderująca(tworząca) elementy z odpowiednimi parametrami (jak klasa i id(które jest sterowane za pomocą pętli)) do opt_container
 function render() {
     for (let i = 1; i <= 6; i++) {
-        console.clear()
         const opt = document.createElement('div')
         opt.classList.add('option')
         opt.setAttribute('id', 'option' + i)
@@ -19,7 +18,6 @@ const opt1 = [document.querySelector('#option1'), document.querySelector('#optio
 function option() {
     const tab = ["listing commands", "directory commands", "system commands", "hardware commands", "files commands", "software commands"];
     for (let i = 0; i <= opt1.length; i++) {
-        console.clear()
         opt1[i].setAttribute('onClick', 'eventclicker()')
         opt1[i].innerHTML = '<p>' + tab[i].toUpperCase() + '</p>'
     }
@@ -30,13 +28,6 @@ option()
 function eventclicker() {
     const sub = ["ls.html", "dir.html", "os.html", "hw.html", "fil.html", "soft.html"]
     for (let i = 0; i <= opt1.length; i++) {
-        console.clear()
         opt1[i].addEventListener("click", () => { window.open(sub[i], "_self") })
     }
-    // opt1[0].addEventListener("click", () => { window.open(sub[0], "_self") })
-    // opt1[1].addEventListener("click", () => { window.open(sub[1], "_self") })
-    // opt1[2].addEventListener("click", () => { window.open(sub[2], "_self") })
-    // opt1[3].addEventListener("click", () => { window.open(sub[3], "_self") })
-    // opt1[4].addEventListener("click", () => { window.open(sub[4], "_self") })
-    // opt1[5].addEventListener("click", () => { window.open(sub[5], "_self") })
 }
