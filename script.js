e2adfb6bf88d25b9c225c38618e0d4fba8f42473
Johngtka <johngtka@gmail.com>
2023-01-03 21:23:56 +0100

    .

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const opt_container = document.querySelector('#menu')
 // funkcja renderująca(tworząca) elementy z odpowiednimi parametrami (jak klasa i id(które jest sterowane za pomocą pętli)) do opt_container
 function render() {
-    console.clear()
     for (let i = 1; i <= 6; i++) {
+        console.clear()
         const opt = document.createElement('div')
         opt.classList.add('option')
         opt.setAttribute('id', 'option' + i)
@@ -17,9 +17,9 @@ const opt1 = [document.querySelector('#option1'), document.querySelector('#optio
 
 // Funkcja która przypisuje przekonwertowany text jako (CAPS LOCK) z lokalnej tablicy do elementu opt1. Dodatkowo dodaje do elementów opt1 zdażenie onclick ustawione na inną, niezależną funkcję
 function option() {
-    console.clear()
     const tab = ["listing commands", "directory commands", "system commands", "hardware commands", "files commands", "software commands"];
     for (let i = 0; i <= opt1.length; i++) {
+        console.clear()
         opt1[i].setAttribute('onClick', 'eventclicker()')
         opt1[i].innerHTML = '<p>' + tab[i].toUpperCase() + '</p>'
     }
@@ -28,9 +28,9 @@ option()
 
 // Funkcja dopisana jako zdarzenie podczas procesu funkcji option(), wykonująca metode słuchacza zdarzeniowego który otworzy odpowiedni plik z podfolderu /subpages na tej samej karcie przeglądarki
 function eventclicker() {
-    console.clear()
     const sub = ["ls.html", "dir.html", "os.html", "hw.html", "fil.html", "soft.html"]
     for (let i = 0; i <= opt1.length; i++) {
+        console.clear()
         opt1[i].addEventListener("click", () => { window.open(sub[i], "_self") })
     }
     // opt1[0].addEventListener("click", () => { window.open(sub[0], "_self") })
